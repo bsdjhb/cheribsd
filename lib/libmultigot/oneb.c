7bc47fd66ebd77aad632f62b082dea0c38c4dc86
John Baldwin <jhb@FreeBSD.org>
2024-08-06 14:49:49 -0400

    libmultigot: Add a simple test library with multiple compartments

---
+++ lib/libmultigot/oneb.c
@@ -0,0 +1,20 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024 Ararat River Consulting, LLC
+ *
+ * This software was developed by SRI International, the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology), and Capabilities Limited under Defense Advanced Research
+ * Projects Agency (DARPA) Contract No. FA8750-24-C-B047 ("DEC").
+ */
+
+#include "libmultigot.h"
+
+#include "internal.h"
+
+void
+decrement_counter(int delta)
+{
+	counter -= delta;
+}
